136e7ff090d4484f7cd4a4e41eb7403acf5094f3
Michael Beckwith <tw2113@gmail.com>
2018-04-24 00:24:41 -0500

    mark tests as incomplete for time being

--- tests/test-optin.php
+++ tests/test-optin.php
@@ -13,10 +13,12 @@ class ConstantContact_Optin_Test extends WP_UnitTestCase {
 	}
 
 	function test_class_access() {
+		$this->markTestIncomplete();
 		$this->assertTrue( constant_contact()->optin instanceof ConstantContact_Optin );
 	}
 
 	function test_can_track() {
+		$this->markTestIncomplete();
 		$optin = constant_contact()->optin;
 		// replace this with some actual testing code
 		$this->assertFalse( $optin->can_track() );
