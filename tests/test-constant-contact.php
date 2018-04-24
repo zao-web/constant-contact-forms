2a52450fc59f93204b036cb13f47c60e4958e4ea
Michael Beckwith <tw2113@gmail.com>
2018-04-24 13:45:22 -0500

    add new file for core class. Will need to figure out how to handle the singleton.

---
+++ tests/test-constant-contact.php
@@ -0,0 +1,18 @@
+<?php
+	/**
+	 * @package ConstantContact_Tests
+	 * @subpackage Updates
+	 * @author Pluginize
+	 * @since 1.0.0
+	 */
+
+class ConstantContact_Core_Test extends WP_UnitTestCase {
+
+	function setup() {
+		parent::setup();
+	}
+
+	function teardown() {
+		parent::teardown();
+	}
+}
