7beb8d92703a840bf989ef8ff8ddf86046664f1a
Michael Beckwith <tw2113@gmail.com>
2018-04-24 21:29:25 -0500

    outline our planned methods to test

--- tests/test-api.php
+++ tests/test-api.php
@@ -17,6 +17,66 @@ class ConstantContact_API_Test extends WP_UnitTestCase {
 		$this->assertTrue( class_exists( 'ConstantContact_API' ) );
 	}
 
+	function test_get_api_token() {
+
+	}
+
+	function test_get_account_info() {
+
+	}
+
+	function test_get_contacts() {
+
+	}
+
+	function test_get_lists() {
+
+	}
+
+	function test_get_list() {
+
+	}
+
+	function test_add_list() {
+
+	}
+
+	function test_update_list() {
+
+	}
+
+	function test_delete_list() {
+
+	}
+
+	function test_add_contact() {
+
+	}
+
+	function test_set_contact_properties() {
+
+	}
+
+	function test_api_error_message() {
+
+	}
+
+	function test_is_connected() {
+
+	}
+
+	function test_get_connect_link() {
+
+	}
+
+	function test_get_signup_link() {
+
+	}
+
+	function test_get_disclosure_info() {
+
+	}
+
 	function teardown() {
 		parent::teardown();
 	}
