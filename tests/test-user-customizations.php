475c5aab93aa391186483f6711dce0edc6b51f99
Scott Tirrell <scott.tirrell@webdevstudios.com>
2018-05-02 16:59:13 -0400

    Added test for process_form_success function.

--- tests/test-user-customizations.php
+++ tests/test-user-customizations.php
@@ -10,10 +10,39 @@ class ConstantContact_User_Customizations_Test extends WP_UnitTestCase {
 
 	function setup() {
 		parent::setup();
+
+		$this->user_customizations = new ConstantContact_User_Customizations( constant_contact() );
 	}
 
 	function test_class_exists() {
 		$this->assertTrue( class_exists( 'ConstantContact_User_Customizations' ) );
+	}
+
+	/**
+	 * Test the returned success message from a submission.
+	 * 1) Test getting the default message back from the form.
+	 * 2) Test getting a custom message back when overriding the default message.
+	 */
+	function test_process_form_success() {
+		$success_message = 'Test success message';
+		$custom_message  = 'This is a custom message';
+
+		// Create a form to test with.
+		$form_id = $this->factory->post->create( array(
+			'post_type' => 'ctct_forms',
+			'meta_input' => [
+				'_ctct_form_submission_success' => 'Test success message',
+			],
+		) );
+
+		$result = $this->user_customizations->process_form_success( '', $form_id );
+		$this->assertEquals( $result, $success_message, "With no custom, we should get the form's success message." );
+
+		$result = $this->user_customizations->process_form_success( $custom_message, $form_id );
+		$this->assertEquals( $result, $success_message,
+			'With a custom message passed in, we should get the custom message back.'
+		);
+
 	}
 
 	function teardown() {
