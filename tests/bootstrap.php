d1fd1cb41167eae04bdb473a7ad87d1ccaaae51b
Michael Beckwith <tw2113@gmail.com>
2019-09-26 18:54:01 -0500

    want to attempt moving away from having to scaffold an entire WP install for our tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,6 +6,8 @@
  * @since 1.0.0
  */
 
+require_once __DIR__ . '/../vendor/autoload.php';
+
 /**
  * Bootstrap the plugin unit testing environment.
  *
@@ -14,26 +16,4 @@
  * @package wordpress-plugin-tests
  */
 
-// Support for:
-// 1. `WP_DEVELOP_DIR` environment variable
-// 2. Plugin installed inside of WordPress.org developer checkout
-// 3. Tests checked out to /tmp
-
-if ( false !== getenv( 'WP_DEVELOP_DIR' ) ) {
-	$test_root = getenv( 'WP_DEVELOP_DIR' );
-} elseif ( false !== getenv( 'WP_TESTS_DIR' ) ) {
-	$test_root = getenv( 'WP_TESTS_DIR' );
-} elseif ( file_exists( '../../../../tests/phpunit/includes/bootstrap.php' ) ) {
-	$test_root = '../../../../tests/phpunit';
-} elseif ( file_exists( '/tmp/wordpress-tests-lib/includes/bootstrap.php' ) ) {
-	$test_root = '/tmp/wordpress-tests-lib';
-}
-require_once $test_root . '/includes/functions.php';
-
-function _test_constant_contact_manually_load_plugin() {
-	require dirname( dirname( __FILE__ ) ) . '/constant-contact-forms.php';
-}
-
-tests_add_filter( 'muplugins_loaded', '_test_constant_contact_manually_load_plugin' );
-
-require $test_root . '/includes/bootstrap.php';
+\WP_Mock::bootstrap();
