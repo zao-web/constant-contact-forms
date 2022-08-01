ec309d5f973f9b49a998aed4ab1526a5681f6019
unknown <faisalashfaq81z@gmail.com>
2022-08-01 15:36:54 +0500

    Encryption added and Middleware updates

--- includes/class-middleware.php
+++ includes/class-middleware.php
@@ -129,6 +129,34 @@ class ConstantContact_Middleware {
 	}
 
 	/**
+	 * encrypt or decrypts given string with chosen cipher method e.g. AES-256-CB
+	* @param null $action
+	* @param null $string
+	* 
+	* @return string encrypted or decrypted data in base64 OpenSSL format
+	*/
+
+	function encrypt_decrypt($action, $string) {
+		
+		$output = false;
+		$encrypt_method = "AES-256-CBC";
+		$secret_key = 'WS-SERVICE-KEY';
+		$secret_iv = 'WS-SERVICE-VALUE';
+		// hash
+		$key = hash('sha256', $secret_key);
+		// iv - encrypt method AES-256-CBC expects 16 bytes - else you will get a warning
+		$iv = substr(hash('sha256', $secret_iv), 0, 16);
+		if ($action == 'encrypt') {
+			$output = base64_encode(openssl_encrypt($string, $encrypt_method, $key, 0, $iv));
+		} else {
+			if ($action == 'decrypt') {
+				$output = openssl_decrypt(base64_decode($string), $encrypt_method, $key, 0, $iv);
+			}
+		}
+		return $output;
+	}
+
+	/**
 	 * Verify a returned request from the auth server, and save the returned token.
 	 *
 	 * @since  1.0.0
